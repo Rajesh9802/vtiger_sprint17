ed41390f740b6d84a672c4951acc6b7a514a680d
Rajesh9802 <125641408+Rajesh9802@users.noreply.github.com>
2024-02-23 14:07:40 +0530

    Update DemoClass.java

--- Vtiger/src/test/java/JDBC/DemoClass.java
+++ Vtiger/src/test/java/JDBC/DemoClass.java
@@ -5,7 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("Demo Push From New Branch");
 		System.out.println("Demo Changing in Global");
-			
+		System.out.println("Demo My Name is Raj");	
 
 	}
 
